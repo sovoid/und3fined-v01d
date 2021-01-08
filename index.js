c8713901adf638bd586790c89d5d74434e8392e0
und3fined-v01d <soham.parekh1998@gmail.com>
2021-01-09 04:01:42 +0530

    ptch: add scripts for local development

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-require('dotenv').config()
+if (process.env.NODE_ENV === 'local') {
+  require('dotenv').config()
+}
+
 const algoliasearch = require('algoliasearch')
 const ejs = require('ejs')
 const { promises: fs } = require('fs')
